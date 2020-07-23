6228861240c046527ee97c17ec1dd2cef4eaa41d
Artem Ershov <artem_ershov@epam.com>
2020-07-23 16:37:57 +0300

    Updates

---
+++ precache-manifest.2034494a46ff5baa72b9c07eb6c9326e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f6a5245b2324a5009fcbed4cec26020",
+    "url": "/TicTacToe/index.html"
+  },
+  {
+    "revision": "cb78234d189da3f764dc",
+    "url": "/TicTacToe/static/css/main.4816fb40.chunk.css"
+  },
+  {
+    "revision": "3096af9771085dd4b06c",
+    "url": "/TicTacToe/static/js/2.368069f8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TicTacToe/static/js/2.368069f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb78234d189da3f764dc",
+    "url": "/TicTacToe/static/js/main.5443bb11.chunk.js"
+  },
+  {
+    "revision": "ebdbc3f3c36f0e8a7009",
+    "url": "/TicTacToe/static/js/runtime-main.3e2f971d.js"
+  }
+]);
