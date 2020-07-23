605e43e4a411735dc7868e65229115c353f8c7fd
Artem Ershov <artem_ershov@epam.com>
2020-07-23 16:01:00 +0300

    refactoring

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,7 +6,8 @@ class App extends Component {
     super(props);
     this.state = {
       square: Array(9).fill(null),
-      counter: 0
+      counter: 0,
+      message: ''
     };
     this.winner_lines = [
       [0, 1, 2],
@@ -28,22 +28,39 @@ class App extends Component {
       const winner_line = this.winner_lines[index];
 
       if (current_array[winner_line[0]] === last_click_value &&
-        current_array[winner_line[1]] == last_click_value &&
+        current_array[winner_line[1]] === last_click_value &&
         current_array[winner_line[2]] === last_click_value) {
 
         setTimeout(() => {
-          alert(last_click_value + " WIN!!!");
-          document.querySelectorAll('.item-value').forEach(el=>{if(el.classList.contains('opacity-enabled')){
-            el.classList.remove('opacity-enabled');
-          }});;
-          this.setState({
-            square: Array(9).fill(null),
-            counter: 0
-          })
+          this.gameMessageAdd(`${last_click_value} win!!! Try Once Again! `)
+         
+          setTimeout(() => {
+            this.setState({
+              square: Array(9).fill(null),
+              counter: 0
+            });
+            this.gameMessageRemove();
+            document.querySelectorAll('.item-value').forEach(el => {
+              if (el.classList.contains('opacity-enabled')) {
+                el.classList.remove('opacity-enabled');
+              }
+            });
+          }, 3000)
+
         }, 500)
       }
 
     }
+  }
+
+  gameMessageAdd = message => {
+    this.setState({ message: message });
+    document.querySelector('.notification').classList.add('opacity-enabled');
+  }
+
+  gameMessageRemove = () => {
+    this.setState({ message: "" });
+    document.querySelector('.notification').classList.remove('opacity-enabled');
   }
 
   checkEmptyFilelds = current_square_array => {
@@ -55,11 +72,15 @@ class App extends Component {
     }
     if (count === 0) {
       setTimeout(() => {
-         alert("DRAW !!! Try once again!!!")
-        this.setState({
-          square: Array(9).fill(null),
-          counter: 0
-        })
+        this.gameMessageAdd("DRAW !!! Try Once Again")
+        setTimeout(() => {
+          this.setState({
+            square: Array(9).fill(null),
+            counter: 0
+          });
+          this.gameMessageRemove();
+        }, 3000)
+
       }, 500)
     }
   }
@@ -69,7 +90,7 @@ class App extends Component {
     let current_value = event.target.getAttribute('data');
     let current_square_array = this.state.square;
 
-console.log(event.target);
+
     if (!current_square_array[current_value]) {
       if (this.state.counter % 2 === 0) {
         event.target.querySelector('.item-value').classList += ' opacity-enabled';
@@ -91,12 +112,14 @@ console.log(event.target);
     return (
       <div className="App">
         <header className="App-header">
-          <div className="app-square">
+          <div className="app-square" id="app-square">
             {
               this.state.square.map((el, index) => <span onClick={this.clickHandler} data={index}
                 className="app-square__item" key={`item${index}`}><span className="item-value">{el}</span></span>)
             }
           </div>
+
+          <span class="notification">{this.state.message}</span>
         </header>
 
       </div>
